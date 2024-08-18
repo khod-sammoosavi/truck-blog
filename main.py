47a309cd6ac2241d06e8c8a7612bd0ea5251155d
SamMoosavi <hi.sammoosavi@gmail.com>
2024-08-18 16:16:09 +0330

    feat(main.py): added BASE_DIRectory.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from settings import wsgi
+
+BASE_DIR = os.path.join(os.path.dirname(__file__))
 
 if __name__ == "__main__":
     wsgi.app.run(host="0.0.0.0", port="5000", debug=True, load_dotenv=True)
