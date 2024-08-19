ab4eb095c6714d85ffb2425f26c3808eb5e6acc9
SamMoosavi <hi.sammoosavi@gmail.com>
2024-08-19 19:59:46 +0330

    feat(wsgi.py): register admin blueprint.

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -1,7 +1,8 @@
 import os
+from main import BASE_DIR
 from flask import Flask
 from home import Home
-from main import BASE_DIR
+from admin import Admin
 
 template_dir = os.path.join(BASE_DIR, "templates")
 static_dir = os.path.join(BASE_DIR, "static")
@@ -9,5 +10,7 @@ static_dir = os.path.join(BASE_DIR, "static")
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 app.config.from_pyfile("settings", silent=True)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.register_blueprint(Home)
+app.register_blueprint(Admin)
